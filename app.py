559a0843f65fd3ec0d15f79fe678ff20aaf2cd34
sksarvesh007 <bt22csd002@iiitn.ac.in>
2024-09-04 14:45:40 +0530

    chore: Update flask_app import path in app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from code_templates.webapp.simple_html import simple_html_template 
-from code_templates.flask_app import generate_flask_template
+from code_templates.backend.flask_app import generate_flask_template
 from code_templates.webapp.react_js_template import react_js_app_template
 simple_html_template()
 generate_flask_template()
