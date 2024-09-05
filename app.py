2a4d7de8aafbec3de996f950b725e9e4fefccb84
sksarvesh007 <bt22csd002@iiitn.ac.in>
2024-09-05 09:10:19 +0530

    added typescript template

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from code_templates.webapp.simple_html import simple_html_template 
 from code_templates.backend.flask_app import generate_flask_template
 from code_templates.webapp.react_js_template import react_js_app_template
-simple_html_template()
-generate_flask_template()
-react_js_app_template()
+from code_templates.webapp.react_ts_template import react_ts_app_template
+simple_html_template("lmao")
+generate_flask_template("lmao")
+react_js_app_template("lmao")
+react_ts_app_template("lmao")
